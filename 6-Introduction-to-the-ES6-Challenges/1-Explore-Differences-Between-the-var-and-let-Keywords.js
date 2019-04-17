e678877efffb47db73abf12b28def1af454e56fd
JJ <justinj1995@outlook.com>
2019-04-17 16:06:06 -0700

    Module 3 is completed.

---
+++ 6-Introduction-to-the-ES6-Challenges/1-Explore-Differences-Between-the-var-and-let-Keywords.js
@@ -0,0 +1,11 @@
+//the let keyword can be used instead of var when a variable name can only be declared once.
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
